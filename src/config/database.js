6d578d6856da2d0e76bcd9cbcdd73147c5dc16a9
loki <stevloki@ymail.com>
2020-03-21 16:56:54 +0700

    database setup

--- src/config/database.js
+++ src/config/database.js
@@ -1,2 +1,7 @@
-const mongoose = require('mongoose');
-
+// const mongoose = require('mongoose');
+module.exports = {
+    HOST: "us-cdbr-iron-east-02.cleardb.net",
+    USER: "bb95048ff9f481",
+    PASSWORD: "b55ca3ac",
+    DB: "heroku_86d3a3f76264125"
+  };
